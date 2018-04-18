72d001b835426ac48a7dc8e6df4271ac8af1453a
Phil <p_pepper@mac.com>
2018-04-18 11:54:18 +0100

    Initial commit.

---
+++ Weather Here/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Here
+//
+//  Created by Phil Pepper on 18/04/2018.
+//  Copyright © 2018 Emerald River. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Weather HereTests/Weather_HereTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_HereTests.swift
+//  Weather HereTests
+//
+//  Created by Phil Pepper on 18/04/2018.
+//  Copyright © 2018 Emerald River. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_Here
+
+class Weather_HereTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
