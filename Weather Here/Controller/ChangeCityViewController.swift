37a31456bd3cf4a5bc99912d769eee3fbc99381f
Phil <p_pepper@mac.com>
2018-04-18 12:33:18 +0100

    Adding View Controllers.

---
+++ Weather Here/Controller/ChangeCityViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChangeCityViewController.swift
+//  Weather Here
+//
+//  Copyright © 2018 Emerald River. All rights reserved.
+//
+
+import UIKit
+
+class ChangeCityViewController: UIViewController {
+    
+}
---
+++ Weather Here/Controller/WeatherViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherViewController.swift
+//  Weather Here
+//
+//  Copyright © 2018 Emerald River. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+ 
+}
