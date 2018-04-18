6da3c7e602603bae532dc55ea7b82227a6fb5591
Phil <p_pepper@mac.com>
2018-04-18 20:19:49 +0100

    Implementing ChangeCityViewController buttons. Adding warning label to ChangeCity view.

--- Weather Here/Controller/ChangeCityViewController.swift
+++ Weather Here/Controller/ChangeCityViewController.swift
@@ -13,14 +13,35 @@ protocol ChangeCityDelegate {
 
 class ChangeCityViewController: UIViewController {
 
+    @IBOutlet var changeCityTextfield: UITextField!
+    @IBOutlet var warningLabel: UILabel!
+    
     var delegate: ChangeCityDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func getWeatherButtonTapped(sender: UIButton) {
+        guard let cityName = changeCityTextfield.text, cityName != "" else {
+            warningLabel.text = "Please enter a city name below"
+            return
+        }
+        delegate?.changeCityName(city: changeCityTextfield.text!)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func backButtonTapped(sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func configureLabels() {
+        warningLabel.text = ""
+    }
+    
 }
