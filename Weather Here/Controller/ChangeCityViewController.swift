08a95e83b64bb6498b8147c887ab05657ed5cde1
Phil <p_pepper@mac.com>
2018-04-19 07:49:08 +0100

    Adding country picker to Change City View. Implementing country picker.

--- Weather Here/Controller/ChangeCityViewController.swift
+++ Weather Here/Controller/ChangeCityViewController.swift
@@ -15,13 +15,18 @@ class ChangeCityViewController: UIViewController {
 
     @IBOutlet var changeCityTextfield: UITextField!
     @IBOutlet var warningLabel: UILabel!
+    @IBOutlet var countryPicker: UIPickerView!
     @IBOutlet var getWeatherButton: UIButton!
     
+    var countryCode: String = "UK"
+    var countryKeys = [String]()
+    var countryValues = [String]()
     var delegate: ChangeCityDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTextField()
+        configureCountryCodes()
         configureGestures()
         configureButtons()
     }
@@ -47,7 +52,14 @@ class ChangeCityViewController: UIViewController {
     }
     
     private func changeCityName(_ cityName: String) {
-        delegate?.changeCityName(city: cityName)
+        var newCityName = ""
+        if !cityName.contains(",") {
+            newCityName = cityName.trimmingCharacters(in: .whitespaces).appending(", \(countryCode)")
+        }
+        else {
+            newCityName = cityName
+        }
+        delegate?.changeCityName(city: newCityName)
         dismissKeyboard()
         self.dismiss(animated: true, completion: nil)
     }
@@ -55,7 +67,17 @@ class ChangeCityViewController: UIViewController {
     //MARK:- Configure
     private func configureTextField() {
         changeCityTextfield.placeholder = NSLocalizedString("lEnterCityNameTextField", comment: "")
-        changeCityTextfield.delegate = self
+    }
+    
+    private func configureCountryCodes() {
+        guard let url = Bundle.main.url(forResource: "countryCodes", withExtension: "json") else {fatalError()}
+        guard let data = try? Data(contentsOf: url) else {fatalError()}
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: []), let results = json as? [Any] else {fatalError()}
+        for countryCode in results {
+            guard let countryCode = countryCode as? [String: String] else {continue}
+            countryKeys.append(countryCode["Code"]!)
+            countryValues.append(countryCode["Name"]!)
+        }
     }
     
     private func configureGestures() {
@@ -72,7 +94,36 @@ class ChangeCityViewController: UIViewController {
     }
 }
 
-//MARK: - UITextView Extension
+//MARK:- UIPicker Extensions
+extension ChangeCityViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countryValues.count
+    }
+    
+}
+
+extension ChangeCityViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countryValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        countryCode = countryKeys[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let title = countryValues[row]
+        let colourTitle = NSAttributedString(string: title, attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        
+        return colourTitle
+    }
+}
+
+//MARK:- UITextView Extension
 extension ChangeCityViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         let cityName = textField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
